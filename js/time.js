eba8a6c7ff0bf6e7981f9730c2b1b4bf5af504c1
5Lunes <lucamevis@gmail.com>
2026-07-13 23:28:00 +0200

    détails

--- js/time.js
+++ js/time.js
@@ -1,7 +1,7 @@
 function updateClock() {
     const options = { timeZone: "Europe/Brussels", hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" };
     const formatter = new Intl.DateTimeFormat("fr-FR", options);
-    document.getElementById("clock").textContent = formatter.format(new Date()) + " UTC+1";
+    document.getElementById("clock").textContent = formatter.format(new Date()) + " UTC+2";
     document.getElementById("clock_mobile").textContent = formatter.format(new Date());
 }
 setInterval(updateClock, 1000);
